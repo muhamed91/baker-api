8d0b19ad136240660351b8de8b3a3eaecb73b3c8
muhamed91 <r_muhamed@live.com>
2021-01-11 21:56:52 +0100

    CORS- Solution

--- public/index.php
+++ public/index.php
@@ -15,4 +15,12 @@ $app->get('/product', function (Request $request, Response $response, array $arg
 
     return $response->withJson($product);
 });
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 $app->run();
